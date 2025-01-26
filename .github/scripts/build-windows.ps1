cc65502e50c24f0d7ee91cc743564a683e8c38d2
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-01-26 14:40:21 -0500

    Test on windows too

---
+++ .github/scripts/build-windows.ps1
@@ -0,0 +1,19 @@
+# Clone vcpkg repository
+git clone https://github.com/microsoft/vcpkg.git
+Set-Location -Path vcpkg
+./bootstrap-vcpkg.bat
+Set-Location -Path ..
+
+# Set environment variables
+$env:VCPKG_ROOT = (Get-Location).Path + "\vcpkg"
+$env:PATH = $env:VCPKG_ROOT + ";" + $env:PATH
+
+# Create build directory and configure
+New-Item -ItemType Directory -Path build -Force
+Set-Location -Path build
+cmake --preset=x64-debug ..
+
+# Build the project
+Set-Location -Path ..\out\build\x64-debug
+ninja
+Set-Location -Path ..\..\..
