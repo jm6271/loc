e81ff398de106b07b636ab9129ceaf288a16b491
jm6271 <107654654+jm6271@users.noreply.github.com>
2025-01-25 22:00:54 -0500

    fix shell script

--- .github/scripts/build-linux.sh
+++ .github/scripts/build-linux.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
+# Clone vcpkg repository
 git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.sh
 cd ..
 export VCPKG_ROOT=$(pwd)/vcpkg
-export PATH=$(VCPKG_ROOT):$(PATH)
+export PATH=${VCPKG_ROOT}:${PATH}
 
-sudo apt install ninja-build -y
-
+# Create build directory and configure
 mkdir -p build
 cd build
-export VCPKG_ROOT="$VCPKG_ROOT"
 cmake --preset=linux-debug ..
+
+# Build the project
 cd ../out/build/linux-debug
 ninja
 cd ../../..
